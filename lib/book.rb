902e80f61579bbd7f0f52caa9785d923e7233d28
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-24 23:36:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,4 +23,16 @@ end
 def page_count
   @page_count
 end
+
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
 end
